b77624e3066c0351ba17ff0f39cedc4ec2333a56
brycesharrits <brycesharrits@gmail.com>
2022-02-07 20:28:06 -0500

    started on payment caclulation

--- src/components/AmortizationDisplay.vue
+++ src/components/AmortizationDisplay.vue
@@ -1,18 +1,36 @@
 <template>
-  <div class="row">
-    <div class="col-8">
-      <p>graph and stuff</p>
-    </div>
-    <div class="col-4">
-      <div class="card">
-            <h5>Refine Your Results</h5>
-            <input v-model="location" placeholder="Atlanta, GA">
-            <input v-model="homeValue" placeholder="$300,000">
-            <input v-model="interestRate" placeholder="4.1%">
-            <input v-model="downPayment" placeholder="$60,000">
-            <input v-model="startDate" placeholder="02/10/2022">
-            <input v-model="loanTerm" placeholder="30 years">
+  <div class="card">
+    <div class="row">
+      <div class="col-8">
+        <div class="card p-3">
+          <p>Monthly principal and interest</p>
+          <h2>{{monthly}}</h2>
         </div>
+      </div>
+      <div class="col-4">
+        <div class="card p-3">
+          <h5>Refine Your Results</h5>
+
+          <label class="form-label mb-0" style="text-align:left">Location</label>
+          <input v-model="location" class="form-control mb-2" placeholder="Atlanta, GA"> <!--Need some kind of service to pull locations-->
+
+          <label class="form-label mb-0" style="text-align:left">Home value</label>
+          <input v-model="homeValue" class="form-control mb-2" placeholder="$300,000"> <!--v-mask?-->
+
+          <label class="form-label mb-0" style="text-align:left">Interest rate</label>
+          <input v-model="interestRate" class="form-control mb-2" placeholder="4.1%"> <!--v-mask?-->
+
+          <label class="form-label mb-0" style="text-align:left">Down payment</label>
+          <input v-model="downPayment" class="form-control mb-2" placeholder="$60,000"> <!--v-mask?-->
+
+          <label class="form-label mb-0" style="text-align:left">Start date</label>
+          <input v-model="startDate" class="form-control mb-2" placeholder="02/10/2022"> <!--v-mask? or date picker-->
+
+          <label class="form-label mb-0" style="text-align:left">Loan term (years)</label>
+          <input v-model="loanTerm" class="form-control mb-2" placeholder="30"> <!--v-mask?-->
+
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -29,7 +47,19 @@ export default Vue.extend({
       interestRate: null,
       downPayment: null,
       startDate: null,
-      loanTerm: null
+      loanTerm: 30
+    }
+  },
+  computed: {
+    monthly () { // might need to convert interest rate to decimal?
+      let numPayments = 0
+      if (this.loanTerm) {
+        numPayments = 12 * this.loanTerm
+      }
+      if (this.interestRate) {
+        return (this.interestRate / (1 - ((1 + this.interestRate) ** (numPayments * -1)))) * (this.homeValue - this.downPayment)
+      }
+      return 'Need more info'
     }
   }
 })
