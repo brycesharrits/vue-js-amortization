2621cca0017405d60eb4a207f3a36b37b8fd8b20
brycesharrits <brycesharrits@gmail.com>
2022-02-07 19:51:35 -0500

    going to use 1 component instead of 2

--- src/views/Amortization.vue
+++ src/views/Amortization.vue
@@ -2,14 +2,7 @@
   <div class="container">
     <h2>Mortgage Amortization Calculator</h2>
     <p>Your amortization schedule will show you how much of your monthly mortgage payments you spend toward principal and interest.</p>
-    <div class="row">
-      <div class="col-8">
-        <AmortizationDisplay msg="Welcome to Your Vue.js + TypeScript App"/>
-      </div>
-      <div class="col-4">
-        <AmortizationEntry msg="Welcome to Your Vue.js + TypeScript App"/>
-      </div>
-    </div>
+    <AmortizationDisplay msg="Welcome to Your Vue.js + TypeScript App"/>
     <img alt="Vue logo" src="../assets/logo.png">
   </div>
 </template>
@@ -17,13 +10,11 @@
 <script>
 import Vue from 'vue'
 import AmortizationDisplay from '@/components/AmortizationDisplay.vue' // @ is an alias to /src
-import AmortizationEntry from '@/components/AmortizationEntry.vue' // @ is an alias to /src
 
 export default Vue.extend({
   name: 'Amortization',
   components: {
-    AmortizationDisplay,
-    AmortizationEntry
+    AmortizationDisplay
   }
 })
 </script>
