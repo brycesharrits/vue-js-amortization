ae376566588c353928e76e0de797c11c2ca95309
brycesharrits <brycesharrits@gmail.com>
2022-02-07 19:18:56 -0500

    just the shell of app

---
+++ src/components/AmortizationDisplay.vue
@@ -0,0 +1,3 @@
+<template>
+  <p>Display</p>
+</template>
---
+++ src/views/Amortization.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <h2>Mortgage Amortization Calculator</h2>
+    <p>Your amortization schedule will show you how much of your monthly mortgage payments you spend toward principal and interest.</p>
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <AmortizationDisplay msg="Welcome to Your Vue.js + TypeScript App"/>
+    <AmortizationEntry msg="Welcome to Your Vue.js + TypeScript App"/>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import AmortizationDisplay from '@/components/AmortizationDisplay.vue' // @ is an alias to /src
+import AmortizationEntry from '@/components/AmortizationEntry.vue' // @ is an alias to /src
+
+export default Vue.extend({
+  name: 'Amortization',
+  components: {
+    AmortizationDisplay,
+    AmortizationEntry
+  }
+})
+</script>
