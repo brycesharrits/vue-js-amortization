558f27410918adb5d96189ce3d9636adb2cb5747
brycesharrits <brycesharrits@gmail.com>
2022-02-10 18:21:37 -0500

    monthly payment calc done

--- src/components/AmortizationDisplay.vue
+++ src/components/AmortizationDisplay.vue
@@ -11,9 +11,6 @@
         <div class="card p-3">
           <h5>Refine Your Results</h5>
 
-          <label class="form-label mb-0" style="text-align:left">Location</label>
-          <input v-model="location" class="form-control mb-2" placeholder="Atlanta, GA"> <!--Need some kind of service to pull locations-->
-
           <label class="form-label mb-0" style="text-align:left">Home value</label>
           <input v-model="homeValue" class="form-control mb-2" placeholder="$300,000"> <!--v-mask?-->
 
@@ -22,9 +19,6 @@
 
           <label class="form-label mb-0" style="text-align:left">Down payment</label>
           <input v-model="downPayment" class="form-control mb-2" placeholder="$60,000"> <!--v-mask?-->
-
-          <label class="form-label mb-0" style="text-align:left">Start date</label>
-          <input v-model="startDate" class="form-control mb-2" placeholder="02/10/2022"> <!--v-mask? or date picker-->
 
           <label class="form-label mb-0" style="text-align:left">Loan term (years)</label>
           <input v-model="loanTerm" class="form-control mb-2" placeholder="30"> <!--v-mask?-->
@@ -42,24 +36,24 @@ export default Vue.extend({
   name: 'AmortizationEntry',
   data: function () {
     return {
-      location: null,
       homeValue: null,
       interestRate: null,
-      downPayment: null,
-      startDate: null,
+      downPayment: 0,
       loanTerm: 30
     }
   },
   computed: {
     monthly () { // might need to convert interest rate to decimal?
       let numPayments = 0
-      if (this.loanTerm) {
-        numPayments = 12 * this.loanTerm
+      if (this.homeValue && this.interestRate && this.loanTerm) {
+        numPayments = 12 * parseInt(this.loanTerm)
+        console.log(this.interestRate)
+        const r = parseFloat(this.interestRate) / 100 / 12
+        console.log(r)
+        return (r / (1 - ((1 + r) ** (numPayments * -1)))) * (parseInt(this.homeValue) - parseInt(this.downPayment)).toFixed(2)
+      } else {
+        return 'Need more info'
       }
-      if (this.interestRate) {
-        return (this.interestRate / (1 - ((1 + this.interestRate) ** (numPayments * -1)))) * (this.homeValue - this.downPayment)
-      }
-      return 'Need more info'
     }
   }
 })
