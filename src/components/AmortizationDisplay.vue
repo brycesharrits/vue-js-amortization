4ab4fe413db7f0ec360a1102dbc1d9a4e505926e
brycesharrits <brycesharrits@gmail.com>
2022-02-10 20:25:28 -0500

    grouped data by year

--- src/components/AmortizationDisplay.vue
+++ src/components/AmortizationDisplay.vue
@@ -20,6 +20,21 @@
               <td>{{payment.stillOwe}}</td>
             </tr>
           </table>
+years
+           <table>
+            <tr>
+              <th>Year</th>
+              <th>Principal</th>
+              <th>Interest</th>
+              <th>Balance</th>
+            </tr>
+            <tr v-for="year in years" :key="year.number">
+              <td>{{year.number}}</td>
+              <td>{{year.principalPayment}}</td>
+              <td>{{year.interestPayment}}</td>
+              <td>{{year.stillOwe}}</td>
+            </tr>
+          </table>
         </div>
       </div>
       <div class="col-4">
@@ -57,6 +72,7 @@ export default Vue.extend({
       downPayment: 0, // user entry
       loanTerm: 30, // user entry
       payments: [],
+      years: [],
       principal: null,
       numPayments: null,
       r: null,
@@ -73,13 +89,13 @@ export default Vue.extend({
         this.r = parseFloat(this.interestRate) / 100 / 12
         console.log(this.r)
         this.monthlyPayment = Math.round((this.r / (1 - ((1 + this.r) ** (this.numPayments * -1)))) * (parseInt(this.homeValue) - parseInt(this.downPayment)))
-        this.calcYears()
+        this.calcPayments()
       } else {
         this.monthlyPayment = null
         // null out everything?
       }
     },
-    calcYears () {
+    calcPayments () {
       this.payments = []
       let remainingBalance = this.principal
 
@@ -90,9 +106,34 @@ export default Vue.extend({
         payment.interestPayment = (index !== 0) ? Math.round(remainingBalance * (this.r)) : 0
         payment.principalPayment = (index !== 0) ? Math.round(this.monthlyPayment - payment.interestPayment) : 0
         this.payments.push(payment)
-        remainingBalance = remainingBalance - this.monthlyPayment
+        remainingBalance = remainingBalance - payment.principalPayment
       }
-      console.log(this.payments)
+      this.calcYears()
+    },
+    calcYears () {
+      this.years = []
+
+      this.payments.forEach((payment) => {
+        const yearNumber = Math.floor(payment.number / 12)
+        let year = {}
+        year.number = yearNumber
+        year.interestPayment = 0
+        year.principalPayment = 0
+        if (this.years.filter((year) => year.number === yearNumber).length) {
+          year = this.years.filter((year) => year.number === yearNumber)[0]
+          this.years = this.years.filter((year) => year.number !== yearNumber)
+        }
+
+        year.interestPayment = parseInt(year.interestPayment) + parseInt(payment.interestPayment)
+        year.principalPayment = parseInt(year.principalPayment) + parseInt(payment.principalPayment)
+        this.years.push(year)
+      })
+
+      let stillOwed = this.principal
+      this.years.forEach((year) => {
+        year.stillOwe = stillOwed
+        stillOwed = stillOwed - year.principalPayment
+      })
     }
   }
 })
